a6ed1cee721cf3a4783f9d55d97e6fb2726849d6
Emily Liu <eliu@hercules-login-4.hpc.msstate.edu>
2025-05-14 02:29:20 +0000

    Move compute_solar_angles and get_defsult_nprocs to top levels

--- dump/mapping/bufr_ssmis.py
+++ dump/mapping/bufr_ssmis.py
@@ -2,77 +2,11 @@
 import os
 import numpy as np
 import numpy.ma as ma
-import pytz
-from pysolar.solar import get_altitude, get_azimuth
-from datetime import datetime, timezone
 from multiprocessing import Pool, cpu_count
 
 import bufr
-from bufr.obs_builder import ObsBuilder, add_main_functions
-
-
-def map_path(map_file_name):
-    script_dir = os.path.dirname(os.path.abspath(__file__))
-    return os.path.join(script_dir, map_file_name)
-
-
-def get_default_nprocs(default=8):
-    """
-    Determine the number of processes to use.
-
-    The number of processes is determined from the environment variables
-    `CPUS_PER_TASK`, `SLURM_CPUS_PER_TASK`, or falls back to the default value.
-
-    Examples from command line:
-        1. Run with 1 slurm task and reserve 12 CPU cores for this one task.
-           In Python, this will spawn 12 worker processes via multiprocessing.Pool.
-           (SLURM_CPUS_PER_TASK = 12)
-           ``srun -n 1 --cpus_per_tasks=12 python bufr_ssmis.py``
-
-        2. Run without slurm and tell Python to spawn 12 worker processes.
-           ``export CPUS_PER_TASKS=12``
-           ``python bufr_ssmis.py``
-
-    :param default: Fallback default if nothing is found. Default is 8.
-    :type default: int
-
-    :return: Number of processes to use.
-    :rtype: int
-    """
-
-    env_nprocs = os.getenv("CPUS_PER_TASK")
-    slurm_nprocs = os.environ.get("SLURM_CPUS_PER_TASK")
-
-    print(f"CPUS_PER_TASK={env_nprocs}, SLURM_CPUS_PER_TASK={slurm_nprocs}")
-
-    try:
-        return int(env_nprocs or slurm_nprocs or default)
-    except ValueError:
-        print(f"[WARN] Invalid environment variable value. Falling back to default: {default}")
-        return default
-
-
-def compute_solar_angles(lat, lon, unix_time):
-    """
-    Compute solar zenith and azimuth angles for a single point.
-
-    :param lat: Latitude in degrees.
-    :type lat: float
-    :param lon: Longitude in degrees.
-    :type lon: float
-    :param unix_time: Unix timestamp (seconds since 1970-01-01T00:00:00Z).
-    :type unix_time: int
-
-    :return: A tuple containing zenith angle and azimuth angle.
-    :rtype: tuple(float, float)
-    """
-
-    dt = datetime.fromtimestamp(int(unix_time), tz=timezone.utc)
-    altitude = get_altitude(lat, lon, dt)
-    azimuth = get_azimuth(lat, lon, dt)
-    zenith = 90.0 - altitude
-
-    return zenith, azimuth
+from bufr.obs_builder import ObsBuilder, add_main_functions, map_path, get_default_nprocs
+from bufr.transforms.geometry import compute_solar_angles
 
 
 MAPPING_PATH = map_path('bufr_ssmis.yaml')
