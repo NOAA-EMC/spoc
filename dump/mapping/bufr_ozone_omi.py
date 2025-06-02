1dd37e75550d2d7f8c1b6df34610f46fc6f5129c
emilyhcliu <emily.liu@noaa.gov>
2025-06-02 10:07:35 -0500

    Remove unnecessary overide of make_obs function.

--- dump/mapping/bufr_ozone_omi.py
+++ dump/mapping/bufr_ozone_omi.py
@@ -24,31 +24,6 @@ class BufrOzoneOmiObsBuilder(BufrOzoneObsBuilder):
     def __init__(self):
         super().__init__(MAPPING_PATH, log_name=os.path.basename(__file__))
 
-    def make_obs(self, comm, input_path):
-
-        # Get container from mapping file first
-        self.log.info('Get container from bufr')
-        container = super().make_obs(comm, input_path)
-
-        self.log.debug(f'container list (original): {container.list()}')
-        self.log.debug(f'all_sub_categories =  {container.all_sub_categories()}')
-        self.log.debug(f'category map =  {container.get_category_map()}')
-
-        # Add new/derived data into container
-        for cat in container.all_sub_categories():
-
-            self.log.debug(f'category = {cat}')
-
-            satId = container.get('satelliteId', cat)
-            if not np.any(satId):
-                self.log.warning(f'category {cat[0]} does not exist in input file')
-
-        # Check
-        self.log.debug(f'container list (updated): {container.list()}')
-        self.log.debug(f'all_sub_categories {container.all_sub_categories()}')
-
-        return container
-
 
 # Add main functions create_obs_file or create_obs_group
 add_main_functions(BufrOzoneOmiObsBuilder)
