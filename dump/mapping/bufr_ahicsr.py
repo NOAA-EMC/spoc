66c311e53a2bc5c2342e5ff86d444b5f87598b4d
Praveen.Kumar <Praveen.Kumar@noaa.gov>
2025-06-11 17:39:29 +0000

    Updated YAML and Python scripts for the Channel dimension and sensorChannelNumber variable

--- dump/mapping/bufr_ahicsr.py
+++ dump/mapping/bufr_ahicsr.py
@@ -1,7 +1,6 @@
 #!/usr/bin/env python3
 import os
 import numpy as np
-import numpy.ma as ma
 
 import bufr
 from bufr.obs_builder import ObsBuilder, add_main_functions, map_path
@@ -14,28 +13,63 @@ class BufrAhicsrObsBuilder(ObsBuilder):
     def __init__(self):
         super().__init__(MAPPING_PATH, log_name=os.path.basename(__file__))
 
+    def compute_sensor_channel_number(self, nlocs, nchannels):
+        """
+        Creates a 2D array of sensor channel numbers with shape (nlocs, nchannels),
+        where each row is [1, 2, ..., nchannels].
+
+        Args:
+            nlocs (int): Number of observation locations.
+            nchannels (int): Number of channels (default is 12).
+
+        Returns:
+            np.ndarray: 2D array with shape (nlocs, nchannels).
+        """
+        return np.tile(np.arange(1, nchannels + 1, dtype=np.int32), (nlocs, 1))
+
     def make_obs(self, comm, input_path):
-        # Get container from mapping file first
+        # Get container from mapping file
         self.log.info('Get container from bufr')
         container = super().make_obs(comm, input_path)
 
         self.log.debug(f'Container list (original): {container.list()}')
-        self.log.debug(f'all_sub_categories =  {container.all_sub_categories()}')
-        self.log.debug(f'Category map =  {container.get_category_map()}')
+        self.log.debug(f'All_sub_categories =  {container.all_sub_categories()}')
+        self.log.debug(f'Category map = {container.get_category_map()}')
 
-        # Add new/derived data into container
+        # Add a new derived variable, sensorChannelNumber into container
         for cat in container.all_sub_categories():
-            self.log.debug(f'category = {cat}')
+            self.log.debug(f'category: {cat}')
 
             satId = container.get('satelliteId', cat)
+            sccf_paths = container.get_paths('sensorCentralFrequency', cat)
+            nlocs = satId.shape[0]  # Number of locations
+            nchannels = 12  # Number of channels 
+            sensor_channel_number = self.compute_sensor_channel_number(nlocs, nchannels)
+            self.log.debug(f'Adding derived variable: sensorChannelNumber for {nlocs} locations')
+            container.add('sensorChannelNumber', sensor_channel_number, sccf_paths, cat)
+
             if not np.any(satId):
                 self.log.warning(f'Category {cat[0]} does not exist in input file')
+                continue  # Skip invalid category
 
         # Final container state
         self.log.debug(f'Container list (updated): {container.list()}')
-        self.log.debug(f'All_sub_categories {container.all_sub_categories()}')
 
         return container
+
+    def _make_description(self):
+        description = super()._make_description()
+
+        description.add_variables([
+            {
+                'name': 'MetaData/sensorChannelNumber',
+                'source': 'sensorChannelNumber',
+                'units': '',
+                'longName': 'Sensor Channel Number',
+            },
+        ])
+
+        return description
 
 
 add_main_functions(BufrAhicsrObsBuilder)
