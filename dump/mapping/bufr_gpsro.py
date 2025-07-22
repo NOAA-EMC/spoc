0da326c6aff8070dfa13e7a10e0567674c95a913
Nicholas Esposito <esposito@hercules-login-4.hpc.msstate.edu>
2025-07-22 15:48:59 -0400

    update to newest versions

--- dump/mapping/bufr_gpsro.py
+++ dump/mapping/bufr_gpsro.py
@@ -7,8 +7,7 @@ import bufr
 from bufr.obs_builder import add_main_functions, map_path
 from bufr_gpsro_obs_builder import BaseGpsroBufrObsBuilder
 
-LOCATION_PROFILE = map_path("./bufr_gpsro_latitude.yaml")
-HEIGHT_PROFILE = map_path("./bufr_gpsro_height.yaml")
+MAPPING_PATH = map_path("./bufr_gpsro.yaml")
 
 # ----------------------------------------------------------------------
 # Concrete implementation
@@ -16,15 +15,12 @@ HEIGHT_PROFILE = map_path("./bufr_gpsro_height.yaml")
 
 
 class GpsroBufrObsBuilder(BaseGpsroBufrObsBuilder):
-
+    print("NICKE 1")
     """Supply the two YAML files to the base class."""
 
     def __init__(self):
-        map_dict = {
-            "loc_profile": str(LOCATION_PROFILE),
-            "height_profile": str(HEIGHT_PROFILE),
-        }
-        super().__init__(map_dict, log_name=os.path.basename(__file__))
+        print("NICKE x2")
+        super().__init__(MAPPING_PATH, log_name=os.path.basename(__file__))
 
 
 add_main_functions(GpsroBufrObsBuilder)
