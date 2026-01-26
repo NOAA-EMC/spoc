83e3c366f853e27994c0c3507d14a640872fd477
Andrew Collard <andrew.collard@noaa.gov>
2026-01-26 23:29:44 +0000

    Final tidy

--- dump/scripts/atmosphere/radiance_ssmis.py
+++ dump/scripts/atmosphere/radiance_ssmis.py
@@ -40,9 +40,7 @@ class BufrSsmisObsBuilder(ObsBuilder):
         for cat in container.all_sub_categories():
             nlocs = container.get('latitude', cat).size
             if nlocs == 0:
-                self.log.warning(f"Skipping empty category {cat[0]}")
-                continue
-        
+                self.log.warning(f"Writing empty file for empty category {cat[0]}")
             self._add_sensor_zenith_and_solar_angles(container, cat)
             self._add_satellite_ascend_descent_orbit(container, cat)
 
@@ -61,41 +59,8 @@ class BufrSsmisObsBuilder(ObsBuilder):
 
     def _make_description(self):
         description = super()._make_description()
-#        self._add_new_variable_descriptions(description)
 
         return description
-
-#    def _add_new_variable_descriptions(self, description):
-#        description.add_variables([
-#            {
-#                'name': 'MetaData/satelliteAscendingFlag',
-#                'source': 'satelliteAscendingFlag',
-#                'longName': 'Satellite Ascending/Descending Orbit Flag (Ascend:1; Descend:-1)',
-#            },
-#            {
-#                'name': 'MetaData/sensorZenithAngle',
-#                'source': 'sensorZenithAngle',
-#                'units': 'degree',
-#                'longName': 'Sensor Zenith Angle',
-#            },
-#            {
-#                'name': 'MetaData/sensorAzimuthAngle',
-#                'source': 'sensorAzimuthAngle',
-#                'units': 'degree',
-#                'longName': 'Sensor Azimuth Angle',
-#            },
-#            {
-#                'name': 'MetaData/solarZenithAngle',
-#                'source': 'solarZenithAngle',
-#                'units': 'degree',
-#                'longName': 'Solar Zenith Angle',
-#            },
-#            {
-#                'name': 'MetaData/solarAzimuthAngle',
-#                'source': 'solarAzimuthAngle',
-#                'units': 'degree',
-#                'longName': 'Solar Azimuth Angle',
-#            }])
 
     def _add_satellite_ascend_descent_orbit(self, container, category):
         """
@@ -110,26 +75,27 @@ class BufrSsmisObsBuilder(ObsBuilder):
         satId = container.get('satelliteId', category)
 
         if not satId.size:
-            add_dummy_variable(container, 'satelliteAscendingFlag', category, 'fieldOfViewNumber')
+            add_dummy_variable(container, 'satelliteAscendingFlag', category, 'satelliteId')
             return
+        else:
+            # Get data from container
+            # ephemeris data - latitude values in order of time
+            first_lat = container.get('latitude1', category)
+            self.log.debug(f'first_lat min/max = {first_lat.min()} {first_lat.max()}')
+            second_lat = container.get('latitude2', category)
+            self.log.debug(f'second_lat min/max = {second_lat.min()} {second_lat.max()}')
+            fovn = container.get('fieldOfViewNumber', category)
+            self.log.debug(f'fovn min/max = {fovn.min()} {fovn.max()}')
 
-        # Get data from container
-        # ephemeris data - latitude values in order of time
-        first_lat = container.get('latitude1', category)
-        self.log.debug(f'first_lat min/max = {first_lat.min()} {first_lat.max()}')
-        second_lat = container.get('latitude2', category)
-        self.log.debug(f'second_lat min/max = {second_lat.min()} {second_lat.max()}')
-        fovn = container.get('fieldOfViewNumber', category)
-        self.log.debug(f'fovn min/max = {fovn.min()} {fovn.max()}')
+            # Determine ascending/descending mode
+            # Compare latitude between the first and second records
+            orbit = np.where(second_lat > first_lat, 1, -1).astype(np.int32)
 
-        # Determine ascending/descending mode
-        # Compare latitude between the first and second records
-        orbit = np.where(second_lat > first_lat, 1, -1).astype(np.int32)
+            self.log.debug(f'orbit min/max = {orbit.min()} {orbit.max()}')
 
-        self.log.debug(f'orbit min/max = {orbit.min()} {orbit.max()}')
+            paths = container.get_paths('fieldOfViewNumber', category)
+            self.log.debug(f'paths = {paths}')
 
-        paths = container.get_paths('fieldOfViewNumber', category)
-        self.log.debug(f'paths = {paths}')
         container.add('satelliteAscendingFlag', orbit, paths, category)
 
     def _add_sensor_zenith_and_solar_angles(self, container, category):
@@ -143,6 +109,7 @@ class BufrSsmisObsBuilder(ObsBuilder):
         """
 
         satId = container.get('satelliteId', category)
+
         if not satId.size:
             dummy_mappings = [
                 ('solarZenithAngle', 'latitude'),
