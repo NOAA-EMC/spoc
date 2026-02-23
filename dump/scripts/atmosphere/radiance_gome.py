84bfa71ab6fc5908752588846537337be0b766b0
hyundeok choi <hyundeok.choi@clogin09.cactus.wcoss2.ncep.noaa.gov>
2026-02-23 16:26:28 -0500

    Add BUFR-D dump configs and scripts

---
+++ dump/scripts/atmosphere/radiance_gome.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+import os
+import bufr
+from bufr.obs_builder import ObsBuilder, add_main_functions, map_path
+
+
+MAPPING_PATH = map_path('radiance_gome.yaml')
+
+
+class BufrAtmsObsBuilder(ObsBuilder):
+    def __init__(self):
+        super().__init__(MAPPING_PATH, log_name=os.path.basename(__file__))
+
+
+add_main_functions(BufrAtmsObsBuilder)
